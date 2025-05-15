49146dc9520e2e0bc3a28531a23055457d0c4637
hanibounoua <bounoua.hani@gmail.com>
2025-05-15 21:36:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const { CLIENT_RENEG_LIMIT } = require('tls');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const ACCESS_TOKEN = '1305a4e5d5eb40'; // Remplace par ton token ipinfo.io
+
+// Pour servir les fichiers statiques (comme les images)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (req, res) => {
+// Obtenir l'IP du client
+const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+console.log(ip)
+try {
+    // Requête vers l'API ipinfo
+    const response = await axios.get(`http://ipinfo.io/${ip}/json?token=${ACCESS_TOKEN}`);
+    const data = response.data;
+
+    // Vérifier le pays
+    if (data.country === 'DZ') {
+        res.send(`
+        <h1>Bienvenue !</h1>
+        <img src="/image1.jpg" alt="Image 1">
+        <img src="/image2.jpg" alt="Image 2">
+    `);
+    } else {
+        console.log(data.country)
+        res.send(`<p>Les images sont masquées pour les utilisateurs non algériens.</p>`);
+    }
+} catch (error) {
+    console.error('Erreur lors de la géolocalisation:', error.message);
+    res.status(500).send('Erreur de géolocalisation IP.');
+}
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
